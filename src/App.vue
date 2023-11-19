9714409b21a05cd260a4be15170f6f06f8555404
nisino25 <nisino25@gmail.com>
2023-11-20 03:02:58 +0900

    final for today

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ export default {
     },
   },
 };
-</script> 
+</script>  
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
